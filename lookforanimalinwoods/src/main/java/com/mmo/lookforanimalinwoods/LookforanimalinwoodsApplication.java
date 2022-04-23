87ce1e5b0a568b9da5ee25d8ae18973430407e4c
minhhoang28 <97076708+minhhoang28@users.noreply.github.com>
2022-04-23 15:07:15 +0700

    init code
    init code

---
+++ lookforanimalinwoods/src/main/java/com/mmo/lookforanimalinwoods/LookforanimalinwoodsApplication.java
@@ -0,0 +1,13 @@
+package com.mmo.lookforanimalinwoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookforanimalinwoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookforanimalinwoodsApplication.class, args);
+	}
+
+}
---
+++ lookforanimalinwoods/src/test/java/com/mmo/lookforanimalinwoods/LookforanimalinwoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmo.lookforanimalinwoods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookforanimalinwoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
